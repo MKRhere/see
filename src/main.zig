db86c7f60aefa403ecaf0d0522988004cddc1a35
Muthu Kumar <muthukumar@thefeathers.in>
2022-07-18 13:14:59 +0530

    fix: openDir -> openIterableDir For: zig 0.10.0-dev.3027+0e26c6149
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- src/main.zig
+++ src/main.zig
@@ -13,7 +13,7 @@ fn copy(reader: File.Reader, writer: File.Writer) !void {
 }
 
 fn ls(cwd: std.fs.Dir, input: []const u8, writer: File.Writer) !void {
-    var it = (try cwd.openDir(input, .{ .iterate = true })).iterate();
+    var it = (try cwd.openIterableDir(input, .{})).iterate();
     while (try it.next()) |entry|
         try writer.print("{s}\n", .{entry.name});
 }
